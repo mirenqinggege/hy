62a40809c187aaf27d518fec9ec75cd10e5f22c6
Hzy <1740394753@qq.com>
2020-09-12 19:31:53 +0800

    11

---
+++ src/main/java/com/m3/fzo/hy/HyApplication.java
@@ -0,0 +1,13 @@
+package com.m3.fzo.hy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyApplication.class, args);
+    }
+
+}
